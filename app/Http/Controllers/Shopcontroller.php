7e7e7c7b737694c3149db1ff5ee87819b21741d4
rahulrawat20 <rahulrawat1561101@gmail.com>
2025-01-13 16:05:54 +0530

    apply filter functionaly for brand and according to price

--- app/Http/Controllers/Shopcontroller.php
+++ app/Http/Controllers/Shopcontroller.php
@@ -3,17 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Switch_;
 
 class Shopcontroller extends Controller
 {
     //
 
     #-- Shop page--#
-    public Function index (){
-        $products = Product::orderby('created_at','DESC')->paginate(12);
-        return view('shop ',compact('products'));
+    public Function index (Request $request){
+        $size = $request->query('size') ? $request->query('size') : 12; 
+        $order_column = "";
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+
+        $f_brands = $request->query('brands');
+
+        Switch($order){
+            case 1:
+                $order_column = "created_at";
+                $o_order = "DESC";
+                break;
+            case 2:
+                $order_column = "created_at";
+                $o_order = "ASC";
+                break;
+            case 3:
+                $order_column = "sale_price";
+                $o_order = "asc";
+                break;
+            case 4:
+                $order_column = "sale_price";
+                $o_order = "desc";
+                break;
+            default:
+                $order_column = "id";
+                $o_order = "desc";
+                break;
+        }
+
+        $brands = Brand::orderBy('name','ASC')->get();
+
+        $products = Product::where(function($query)use($f_brands){
+            $query->whereIn('brand_id',explode(',',$f_brands))->orWhereRaw("'".$f_brands."'=''");
+        })
+        
+        ->orderBy($order_column,$o_order)->paginate($size);
+        return view('shop ',compact('products','size','order','brands','f_brands'));
     }
 
     #-- Product details --#  
